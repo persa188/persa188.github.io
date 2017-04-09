b0300df9f6cfded867881171807ea193c814e06e
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-08 23:55:41 -0400

    added site files

---
+++ js/view.js
@@ -0,0 +1,25 @@
+﻿/*jshint esversion:6*/
+var view = (function () {
+    "use strict";
+    var view = {};
+
+    window.onload = function () {
+       //todo
+    };
+
+    document.getElementById("git").onclick = function () {
+        location.href = 'https://github.com/persa188';
+    };
+
+    document.getElementById("bio").onclick = function () {
+        var modal = document.getElementById('myModal');
+        modal.style.display = "block";
+    };
+
+    document.getElementById("close-modal").onclick = function () {
+        var modal = document.getElementById('myModal');
+        modal.style.display = "none";
+    };
+
+    return view;
+}(window));
