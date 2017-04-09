dbf044245c7b2af827171c593b55e3a4373429e5
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-09 09:51:57 -0400

    a bunch of changes

---
+++ js/view.js
@@ -0,0 +1,27 @@
+var view = (function(){
+  "use strict";
+
+  var view = {};
+
+  //glob vars
+  var modal = document.getElementById('myModal');
+  var span = document.getElementsByClassName("close")[0];
+
+  document.getElementById('aboutme').onclick = function () {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+
+  return view;
+}(window));
