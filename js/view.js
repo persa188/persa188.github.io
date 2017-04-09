9a46b2421d32cc78016bc9792bc8ef04d0dc8c05
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-09 19:25:33 -0400

    added privacy policy required

--- js/view.js
+++ js/view.js
@@ -6,9 +6,15 @@ var view = (function(){
   //glob vars
   var modal = document.getElementById('myModal');
   var span = document.getElementsByClassName("close")[0];
+  var span_pmodal = document.getElementsByClassName("close")[1];
+  var privacy_modal = document.getElementById('privacy-policy');
 
   document.getElementById('aboutme').onclick = function () {
     modal.style.display = "inline-block";
+  }
+
+  document.getElementById('ppolicy').onclick = function () {
+    privacy_modal.style.display = "inline-block";
   }
 
   // When the user clicks on <span> (x), close the modal
@@ -16,10 +22,16 @@ var view = (function(){
       modal.style.display = "none";
   }
 
+  span_pmodal.onclick = function () {
+    privacy_modal.style.display = "none";
+  }
+
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
       if (event.target == modal) {
           modal.style.display = "none";
+      } else if (event.target == privacy_modal) {
+        privacy_modal.style.display = "none";
       }
   }
 
