3ce840add002219719ad16407679ad10b26a1829
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-04-10 08:47:35 -0400

    added twitter modal

--- js/view.js
+++ js/view.js
@@ -8,6 +8,8 @@ var view = (function(){
   var span = document.getElementsByClassName("close")[0];
   var span_pmodal = document.getElementsByClassName("close")[1];
   var privacy_modal = document.getElementById('privacy-policy');
+  var twitter_ico = document.getElementById('twitter_ico');
+  var twitter_modal = document.getElementById('twitter_modal');
 
   document.getElementById('aboutme').onclick = function () {
     modal.style.display = "inline-block";
@@ -26,12 +28,24 @@ var view = (function(){
     privacy_modal.style.display = "none";
   }
 
+ document.querySelector('body').addEventListener('click', function(event) {
+   if (event.target.id.toLowerCase() === 'close_tmodal'){
+     twitter_modal.style.display = "none";
+   }
+ })
+
+  twitter_ico.onclick = function () {
+    twitter_modal.style.display = 'block';
+  }
+
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
       if (event.target == modal) {
           modal.style.display = "none";
       } else if (event.target == privacy_modal) {
         privacy_modal.style.display = "none";
+      } else if (event.target == twitter_modal) {
+        twitter_modal.style.display = 'none';
       }
   }
 
