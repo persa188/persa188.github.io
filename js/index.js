991dd2079dacc25dae0e54edc7933f22e40beae5
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2018-08-04 23:59:59 -0400

    testing offset fix #2

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ var view = (function(){
 
   var scrollTo = function(EID) {
     $('html, body').animate({
-      scrollTop: $(EID).offset().top
+      scrollTop: $(EID).offset().top - 100
     }, 1000);
   }
 
