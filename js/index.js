5c95ce24f3fc9199e37dd0cc8a8ad7b74f511427
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2018-08-09 00:34:06 -0400

    do ppl really need to know about me?"

--- js/index.js
+++ js/index.js
@@ -10,57 +10,17 @@ var view = (function(){
 
   var view = {};
 
-  var top_nav = document.getElementById('topnav');
-  var top_nav_brand = document.getElementById("nav-brand");
-  var top_nav_about = document.getElementById("about-nv-btn");
-  var top_nav_contact = document.getElementById("contact-nv-btn");
+  var c_down = document.getElementById('c-down');
 
-  top_nav_about.onclick = function() {
-    scrollTo("#about");
-  }
-
-  top_nav_contact.onclick = function() {
+  c_down.onclick = function() {
     scrollTo("#contact");
   }
-
-  top_nav_brand.onclick = function() {
-    scrollTo("#main");
-  }
-
-  window.addEventListener('scroll', function(e) {
-    var p = $(window).scrollTop();
-    //for the topnav color & brand visibility & animations
-    if (p) {
-      top_nav.style.background = CLR_BLK;
-      top_nav_brand.style.color = CLR_WHITE;
-    } else {
-      top_nav.style.background = CLR_TRANSPARENT;
-      top_nav_brand.style.color = CLR_TRANSPARENT;
-    }
-
-    //highlight the currently focused section in NAV
-    top_nav_about.className = isScrolledIntoView("#about") ? NAV_ITEM_HIGHLITED : NAV_ITEM;
-    top_nav_contact.className = isScrolledIntoView("#contact") ? NAV_ITEM_HIGHLITED : NAV_ITEM;
-  });
 
   var scrollTo = function(EID) {
     $('html, body').animate({
       scrollTop: $(EID).offset().top - SCROLL_OFFSET
     }, 1000);
   }
-
-  //too lazy to wtite this, stolen from https://goo.gl/YHuvYy
-  var isScrolledIntoView = function(elem)
-  {
-      var docViewTop = $(window).scrollTop();
-      var docViewBottom = docViewTop + $(window).height();
-
-      var elemTop = $(elem).offset().top;
-      var elemBottom = elemTop + $(elem).height();
-
-      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-  }
-
 
   return view;
 }($, window));
